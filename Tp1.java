d26b409a801e3b7c211211b13a79a7080de5bc60
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-30 20:58:51 -0300

    Update Tp1.java

--- Tp1.java
+++ Tp1.java
@@ -2,46 +2,33 @@ public class Tp1 {
     public static void main (String [] args) {
        //Ejercicio 1 
         int num1 = 10, num2 = 20;
-        
-        while (num1 <= num2 ) {
-        System.out.print(num1);
-        num1++;
-    
-if (num1 % 2 == 0) {
-    System.out.println(" El número es impar");
-  }else{
-      System.out.println(" El número es par");
-      
+         while (num1 <= num2 ) {
+          System.out.print(num1);
+          num1++;
+         if (num1 % 2 == 0) {
+          System.out.println(" El número es impar");
+        }else{
+          System.out.println(" El número es par");
+      }
   }
-}
     System.out.println ("----------------");
-
-int num3 = 10, num4 = 20;
-        
+       
+      int num3 = 10, num4 = 20;
         for(int i = num4; i >= num3; i--){
-
-        System.out.print(num4);
-        num4--;
-    
-if (num4 % 2 == 0) {
-    System.out.println(" El número es impar");
-  }else{
-      System.out.println(" El número es par");
-      
+         System.out.print(num4);
+         num4--;
+       if (num4 % 2 == 0) {
+         System.out.println(" El número es impar");
+      }else{
+         System.out.println(" El número es par");  
   }
 }
-
-System.out.println ("---------");
-//Ejercicio 2
-
-int vehiculos = 3, inmuebles = 3;
+    System.out.println ("---------");
+    //Ejercicio 2
+      int vehiculos = 3, inmuebles = 3;
       double mensual = 489.083;
       boolean aeronave = true, activo = true, embarcacion = true, ingreso = true;
-      
-      
-      if ((vehiculos >= 3) || (inmuebles >= 3) || (mensual >= 489.083));
-
-      System.out.println("Ingresos altos");
+        if ((vehiculos >= 3) || (inmuebles >= 3) || (mensual >= 489.083));
+         System.out.println("Ingresos altos");
     }
-
-}
+ }
